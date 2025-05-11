35702d736a2baaa773f88acdc7b85d51f0cf4e8f
muhcho <144784372+muhcho@users.noreply.github.com>
2025-05-11 13:19:12 +0200

    s

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc"; // this on might be "import react from "@vitejs/plugin-react";" in your solution
 
-export default defineConfig({
-  plugins: [react(), svgr()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: "/"
+    };
+
+    if (command !== "serve") {
+        config.base = "/UNIwisewebsite/";
+    }
+
+    return config;
 });
