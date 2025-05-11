2e24925d077df6cd223d27320f99bfce87aa441c
muhcho <144784372+muhcho@users.noreply.github.com>
2025-05-11 13:22:26 +0200

    ss

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/UNIwisewebsite/"}>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
